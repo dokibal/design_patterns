98f87076611aef8fd8e78729b4fddd8c4965ba95
dob4bp <Balazs.Doktor@hu.bosch.com>
2024-02-15 15:32:20 +0100

    Add flyweight pattern example

---
+++ structural/flyweight/forest/Forest.java
@@ -0,0 +1,29 @@
+package flyweight.forest;
+
+
+import javax.swing.*;
+
+import flyweight.trees.Tree;
+import flyweight.trees.TreeType;
+import flyweight.trees.TreeFactory;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        for (Tree tree : trees) {
+            tree.draw(graphics);
+        }
+    }
+}
