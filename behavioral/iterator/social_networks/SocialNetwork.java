842dae3696d1f195216dfb38b27ef1aa1214e7c3
dob4bp <Balazs.Doktor@hu.bosch.com>
2024-03-04 16:31:57 +0100

    Add iterator pattern

---
+++ behavioral/iterator/social_networks/SocialNetwork.java
@@ -0,0 +1,8 @@
+package behavioral.iterator.social_networks;
+
+import behavioral.iterator.iterators.ProfileIterator;
+
+public interface SocialNetwork {
+    ProfileIterator createFriendsIterator(String profileEmail);
+    ProfileIterator createCoworkersIterator(String profileEmail);
+}
